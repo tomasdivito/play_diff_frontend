7b38b9f056ecd2bae86198ed8ae110c51b82d3d4
Tomas Di Vito <tomasdv2@gmail.com>
2019-06-19 13:26:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  const login = () => {
+    fetch('http://localhost:8081/login')
+  }
+
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+function Authorized() {
+  return (
+    <div>
+      <h1>Authorized site</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Index} />
+        <Route path="/auth" exact component={Authorized} />
+      </div>
+    </Router>
   );
 }
 
